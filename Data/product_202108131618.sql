911428d6999b2b462b00123e15c440ef444ee042
EliotSyd <eliot.sydenham@gmail.com>
2021-09-17 20:05:50 +0100

    dbChange

--- Data/product_202108131618.sql
+++ Data/product_202108131618.sql
@@ -1,17 +1,33 @@
 INSERT INTO product (product_id,product_name,selling_price,product_image) VALUES
-	 (1,'Sandal',10.0,8.0),
-	 (2,'Shoe',40.0,35.0),
-	 (3,'Trainer',35.0,30.0),
-	 (4,'Boot',50.0,40.0),
-	 (5,'Slipper',5.0,4.0),
-	 (6,'Shirt',40.0,30.0),
-	 (7,'Polo Shirt',45.0,32.0),
-	 (8,'Jumper',40.0,30.0),
-	 (9,'Hoodie',35.0,25.0),
-	 (10,'Sweatshirt',35.0,25.0);
+	 (1,'Sandal',10.0),
+	 (2,'Shoe',40.0),
+	 (3,'Trainer',35.0),
+	 (4,'Boot',50.0),
+	 (5,'Slipper',5.0),
+	 (6,'Shirt',40.0),
+	 (7,'Polo Shirt',45.0),
+	 (8,'Jumper',40.0),
+	 (9,'Hoodie',35.0),
+	 (10,'Sweatshirt',35.0);
 INSERT INTO product (product_id,product_name,selling_price,product_image) VALUES
-	 (11,'Bomber Jacket',60.0,30.0),
-	 (12,'Denim Jacket',60.0,30.0),
-	 (13,'Leather Jacket',70.0,30.0),
-	 (14,'Parka Jacket',75.0,30.0),
-	 (15,'Harrington Jacket',70.0,30.0);
+	 (11,'Bomber Jacket',60.0),
+	 (12,'Denim Jacket',60.0),
+	 (13,'Leather Jacket',70.0),
+	 (14,'Parka Jacket',75.0),
+	 (15,'Harrington Jacket',70.0);
+INSERT INTO product (product_id,product_name,selling_price,product_image) VALUES
+	 (16, 'Phone Case',10.0),
+	 (17, 'Portable Charger',20.0),
+	 (18, 'Iphone Cable',5.0),
+	 (19, 'Bluetooth Speaker',100.0),
+	 (20, 'Bluetooth Headphones',80.0),
+	 (21, 'Smartwatch',60.0),
+	 (22, 'Flash Drive',20.0),
+	 (23, 'Laptop Bag',15.0),
+	 (24, 'Everyday Backpack',35.0),
+	 (25, 'Torch',25.0),
+	 (26, 'Air Purifier',100.0),
+	 (27, 'Streaming Player',55.0),
+	 (28, 'Wireless Charger',65.0),
+	 (29, '4K TV',700.0),
+	 (30, 'Sound Bar'75.0);
