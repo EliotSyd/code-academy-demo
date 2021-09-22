4ae39673ce7a6cdd9c4bf93c33b5e9095847ecbd
EliotSyd <eliot.sydenham@gmail.com>
2021-09-22 13:48:11 +0100

    change

--- Data/product_202108131618.sql
+++ Data/product_202108131618.sql
@@ -1,35 +1,35 @@
-INSERT INTO CodeAcademySchema.product (product_id,product_name,selling_price,product_image) VALUES
-	 (1,'Sandal',10.0,'/Sandals.jpeg'),
-	 (2,'Shoe',40.0,'/toms-casual-shoes.jpeg'),
-	 (3,'Trainer',35.0,'/Trainers.jpeg'),
-	 (4,'Boot',50.0,'/Boots.jpeg'),
-	 (5,'Slipper',5.0,'/Slippers.webp'),
-	 (6,'Shirt',40.0,'/Shirt.jpeg'),
-	 (7,'Polo Shirt',45.0,'/Polo Shirt.jpeg'),
-	 (8,'Jumper',40.0,'/Jumper.jpeg'),
-	 (9,'Hoodie',35.0,'/Hoodie.jpeg'),
-	 (10,'Sweatshirt',35.0,'/Sweatshirt.jpeg');
+INSERT INTO CodeAcademySchema.product (product_id,product_name,selling_price,product_image,category) VALUES
+	 (1,'Sandal',10.0,'/Sandals.jpeg', 'Footwear'),
+	 (2,'Shoe',40.0,'/toms-casual-shoes.jpeg','Footwear'),
+	 (3,'Trainer',35.0,'/Trainers.jpeg','Footwear'),
+	 (4,'Boot',50.0,'/Boots.jpeg','Footwear'),
+	 (5,'Slipper',5.0,'/Slippers.webp','Footwear'),
+	 (6,'Shirt',40.0,'/Shirt.jpeg','Clothing'),
+	 (7,'Polo Shirt',45.0,'/Polo Shirt.jpeg','Clothing'),
+	 (8,'Jumper',40.0,'/Jumper.jpeg','Clothing'),
+	 (9,'Hoodie',35.0,'/Hoodie.jpeg','Clothing'),
+	 (10,'Sweatshirt',35.0,'/Sweatshirt.jpeg','Clothing');
 
-INSERT INTO CodeAcademySchema.product (product_id,product_name,selling_price,product_image) VALUES
-	 (11,'Bomber Jacket',60.0,'/Bomber Jacket.jpeg'),
-	 (12,'Denim Jacket',60.0,'/Denim Jacket.webp'),
-	 (13,'Leather Jacket',70.0,'/Leather Jacket.jpeg'),
-	 (14,'Parka Jacket',75.0,'/Parka Jacket.jpeg'),
-	 (15,'Harrington Jacket',70.0,'/Harrington Jacket.jpeg');
+INSERT INTO CodeAcademySchema.product (product_id,product_name,selling_price,product_image,category) VALUES
+	 (11,'Bomber Jacket',60.0,'/Bomber Jacket.jpeg','Clothing'),
+	 (12,'Denim Jacket',60.0,'/Denim Jacket.webp','Clothing'),
+	 (13,'Leather Jacket',70.0,'/Leather Jacket.jpeg','Clothing'),
+	 (14,'Parka Jacket',75.0,'/Parka Jacket.jpeg','Clothing'),
+	 (15,'Harrington Jacket',70.0,'/Harrington Jacket.jpeg','Clothing');
 	 
-INSERT INTO CodeAcademySchema.product (product_id,product_name,selling_price,product_image) VALUES
-	 (16, 'Phone Case',10.0,'/Iphone Case.jpeg'),
-	 (17, 'Portable Charger',20.0,'/Portable Charger.jpg'),
-	 (18, 'Iphone Cable',5.0,'/Iphone Cable.jpeg'),
-	 (19, 'Bluetooth Speaker',100.0,'/Bluetooth Speaker.jpeg'),
-	 (20, 'Bluetooth Headphones',80.0,'/Bluetooth Headphones.jpeg'),
-	 (21, 'Smartwatch',60.0,'/Smart Watch.jpg'),
-	 (22, 'Flash Drive',20.0,'/Flash Drive.jpeg'),
-	 (23, 'Laptop Bag',15.0,'/Laptop Bag.jpeg'),
-	 (24, 'Everyday Backpack',35.0,'/Everyday Backpack.jpeg'),
-	 (25, 'Torch',25.0,'/Torch.jpeg'),
-	 (26, 'Air Purifier',100.0,'/Air Purifier.jpeg'),
-	 (27, 'Streaming Player',55.0,'/Streaming Player.jpg'),
-	 (28, 'Wireless Charger',65.0,'/Wireless Charger.jpg'),
-	 (29, '4K TV',700.0,'/4K TV.jpg'),
-	 (30, 'Sound Bar',80.0,'/Sound Bar.jpg');
+INSERT INTO CodeAcademySchema.product (product_id,product_name,selling_price,product_image,category) VALUES
+	 (16, 'Phone Case',10.0,'/Iphone Case.jpeg','Phone Accessories'),
+	 (17, 'Portable Charger',20.0,'/Portable Charger.jpg','Phone Accessories'),
+	 (18, 'Iphone Cable',5.0,'/Iphone Cable.jpeg','Phone Accessories'),
+	 (19, 'Bluetooth Speaker',100.0,'/Bluetooth Speaker.jpeg','Phone Accessories'),
+	 (20, 'Bluetooth Headphones',80.0,'/Bluetooth Headphones.jpeg','Phone Accessories'),
+	 (21, 'Smartwatch',60.0,'/Smart Watch.jpg','Tech Stuff'),
+	 (22, 'Flash Drive',20.0,'/Flash Drive.jpeg','Tech Stuff'),
+	 (23, 'Laptop Bag',15.0,'/Laptop Bag.jpeg','Tech Stuff'),
+	 (24, 'Everyday Backpack',35.0,'/Everyday Backpack.jpeg','Tech Stuff'),
+	 (25, 'Torch',25.0,'/Torch.jpeg','Tech Stuff'),
+	 (26, 'Air Purifier',100.0,'/Air Purifier.jpeg','Tech Stuff'),
+	 (27, 'Streaming Player',55.0,'/Streaming Player.jpg','TV Stuff'),
+	 (28, 'Wireless Charger',65.0,'/Wireless Charger.jpg','Tech Stuff'),
+	 (29, '4K TV',700.0,'/4K TV.jpg','TV Stuff'),
+	 (30, 'Sound Bar',80.0,'/Sound Bar.jpg','TV Stuff');
